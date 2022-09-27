f7e366421aa23cff53893d77fb9adfb5a4a0e7c2
P.B Binu Binu <>
2022-09-27 11:26:24 +0000

    coding

---
+++ set01/problem01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Binu Prasad/n");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,b,sum;
+  a=5,b=6;
+  sum=a+b;
+  printf("%d %d %d",sum,a,b);
+  printf("Binu Prasad/n");
+  return 0;
+}
