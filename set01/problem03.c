6f30fb1b890808067c532d599e745c4fa4ef6991
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2022-11-28 12:19:34 +0000

    problem solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+# include <stdlib.h>
  int sum_func(int a,int b);
 
 
 int main() {
   int a, b,sum;
   printf("Enter the first number");
-  scanf("%d", &a);
+  if(scanf("%d", &a)!=1)
+    {
+    printf("error");
+    exit(0);
+    }
   printf("Enter second number");
-  scanf("%d", &b);
+  if(scanf("%d", &b)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
   sum=sum_func(a,b);
   printf("%d", sum);
 
