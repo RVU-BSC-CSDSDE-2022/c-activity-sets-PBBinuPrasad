80e2b12a8af4eca00a12fd2ae325d80af768fa8f
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2022-12-05 04:29:09 +0000

    problem solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -21,17 +21,12 @@ int input() {
 }
 
 int compare(int a , int b , int c) {
-  if(a>b)
-  {
-    if(a>c){return(a);}
-    else{return(c);}
-    
-  }
-    else
-     {
-    if(b>c){return(b);}
-    else{return(c);}
-     }
+  if ((a>c) && (a>b))
+    return a;
+  if((b>a) && (b>c))
+    return b;
+  if((c>a) && (c>b))
+    return c;
   }
 
 void output(int a , int b , int c , int largest) {
